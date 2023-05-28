c279f0df0763392d6b59ed891b5a4f9fcc60a431
DemoNinja <775819329@qq.com>
2023-05-29 01:36:55 +0800

    矩形内随机生成，方法有点问题

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -13,8 +13,9 @@ public class EnemySpawner : MonoBehaviour
     private float outsideRadius;
     [SerializeField]
     private float spawnCD;
+    int count=0;
 
-    private float time=1.0f;
+    private float time = 1.0f;
 
     private void Awake()
     {
@@ -41,6 +42,16 @@ public class EnemySpawner : MonoBehaviour
             SpawnInRandomInPosition(insideRadius, outsideRadius);
             time = spawnCD;
         }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            
+            print("生成怪物。。。。。。");
+            SpawnInRecPosition(insideRadius, outsideRadius);
+            
+            print("怪物数量--------------："+count);
+            count = 0;
+            time = spawnCD;
+        }
     }
 
     //随机生成怪物，在以自身为圆心，大于inRadius半径的圆，小于outRadius半径的圆的区域内
@@ -53,6 +64,40 @@ public class EnemySpawner : MonoBehaviour
 
         Enemy e = Instantiate(enemy, relatePos, Quaternion.identity);
         EnemyManager._instance.enemies.Add(e);
+
+    }
+
+    Vector2 randomPos;
+    public void SpawnInRecPosition(float inRadius, float outRadius)
+    {
+        List<float> boundary = MapManager._instance.Boundary;
+
+        bool isValidPosition = false;
+
+        int maxAttempts = 100;
+        int attemptCount = 0;
+        Vector2 playerPos = PlayerManager._instance.player.transform.position;
+
+        while (!isValidPosition && attemptCount < maxAttempts)
+        {
+            float randomX = Random.Range(boundary[0], boundary[1]);
+            float randomY = Random.Range(boundary[3], boundary[2]);
+            randomPos = new Vector2(randomX, randomY);
+
+            if (inRadius <= Vector2.Distance(randomPos, playerPos)
+                && Vector2.Distance(randomPos, playerPos) <= outRadius)
+            {
+                isValidPosition = true;
+                count++;
+            }
+            attemptCount++;
+        }
+
+        if (isValidPosition)
+        {
+            Enemy e = Instantiate(enemy, randomPos, Quaternion.identity);
+            EnemyManager._instance.enemies.Add(e);
+        }
 
     }
 }
