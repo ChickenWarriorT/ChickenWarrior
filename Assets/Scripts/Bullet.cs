6f087fa693d423f8ee04087f3395b62ca48333b6
xiangyicheng <xiangyicheng@forfungame.com>
2023-06-12 21:30:01 +0800

    基本解决了子弹转圈问题

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -42,6 +42,8 @@ public class Bullet : MonoBehaviour
     private float flyDistance;
 
     private float flyTime;
+    private float flyTimeMax = 20.0f;
+    private float flyMaxTimer;
 
     private float currentFlyDistance;
 
@@ -49,6 +51,8 @@ public class Bullet : MonoBehaviour
     private bool isTracing;
 
     private Transform target;
+    [SerializeField]
+    private float detectDistance = 0.01f;
 
     private void Awake()
     {
@@ -69,7 +73,7 @@ public class Bullet : MonoBehaviour
         DestoryOnTime();
         //fixedupdate每秒50帧，所以乘以50
         rb.velocity = dir * moveSpeed * 50 * Time.fixedDeltaTime;
-
+        //RayTracingDetect();
         //如果是追踪子弹
         if (isTracing)
         {
@@ -81,13 +85,26 @@ public class Bullet : MonoBehaviour
     //改变子弹飞行轨迹，根据目标单位
     public void ChangeDirection(Transform target)
     {
-        if (target != null)
+        if (IsNotNullOrInActive(target))
         {
             dir = (target.position - transform.position).normalized;
 
             //改变子弹朝向，目标
             transform.rotation = Quaternion.LookRotation(Vector3.forward, dir);
         }
+        else
+        {
+            dir = transform.rotation * Vector2.up;
+        }
+    }
+    //检测target 是空 或者不活动
+    private bool IsNotNullOrInActive(Transform target)
+    {
+        if (target != null && target.gameObject.activeSelf == true)
+        {
+            return true;
+        }
+        return false;
     }
 
     //初始化
@@ -100,6 +117,7 @@ public class Bullet : MonoBehaviour
         transform.position = startPos;
         leftBounce = bounceCount;
         ChangeDirection(this.target);
+        flyMaxTimer = flyTimeMax;
         flyTime = ConvertFlyDistanceToTime(flyDistance, rb.velocity.magnitude);
         Debug.Log("飞行时间:" + flyDistance + ";" + flyTime);
         isMove = true;
@@ -137,7 +155,12 @@ public class Bullet : MonoBehaviour
     private void DestoryOnTime()
     {
         flyTime -= Time.fixedDeltaTime;
+        flyMaxTimer -= Time.fixedDeltaTime;
         if (flyTime <= 0.0f)
+        {
+            gameObject.SetActive(false);
+        }
+        if (flyMaxTimer <= 0.0f)
         {
             gameObject.SetActive(false);
         }
@@ -149,44 +172,101 @@ public class Bullet : MonoBehaviour
         return distance / speed;
     }
 
+    //射线检测
+    private void RayTracingDetect()
+    {
+        Ray2D ray = new Ray2D(transform.position, dir);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, detectDistance);
+        if (hit.collider != null && hit.collider.CompareTag("Enemy"))
+        {
+            Character character = hit.collider.GetComponent<Character>();
+            if (character != null)
+            {
+                character.TakeDamage(damage);
+                if (leftBounce > 0)
+                {
+                    leftBounce--;
+                    Monster monster = character as Monster;
+                    if (monster != null)
+                    {
+                        //拿到随机一个enemy
+                        var tempEnemy = FindRandomEnemyNotIncludeTarget(monster);
+
+                        if (tempEnemy != null)
+                        {
+                            target = tempEnemy.transform;
+                            print(tempEnemy.name);
+                            ChangeDirection(target);
+                            flyTime = ConvertFlyDistanceToTime(afterbounceDistance, moveSpeed);
+                        }
+                        else
+                        {
+                            print("弹射次数大于0时，未找到怪物，向随机方向发射");
+                            dir = GetRandomDirection();
+                            transform.rotation = Quaternion.LookRotation(Vector3.forward, dir);
+                            Debug.Log("随机飞行方向:" + dir);
+                            target = null;
+                            flyTime = ConvertFlyDistanceToTime(afterbounceDistance, moveSpeed);
+                        }
+                    }
+                    else
+                    {
+                        print("弹跳次数不足，销毁子弹。");
+                        gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+        Debug.DrawRay(ray.origin, ray.direction , Color.green);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
 
         if (other.CompareTag("Enemy"))
         {
             Character character = other.GetComponent<Character>();
-            character.TakeDamage(damage);
-            if (leftBounce > 0)
+            if (character != null)
             {
-                //GetComponent<Collider2D>().enabled = false;
-                leftBounce--;
-                var tempEnemy = FindRandomEnemyNotIncludeTarget((Monster)character);
+                character.TakeDamage(damage);
+                if (leftBounce > 0)
+                {
+                    //GetComponent<Collider2D>().enabled = false;
+                    leftBounce--;
+                    Monster monster = character as Monster;
+                    if (monster != null)
+                    {
+                        var tempEnemy = FindRandomEnemyNotIncludeTarget(monster);
 
-                if (tempEnemy != null)//&& tempEnemy.gameObject.activeSelf == true
-                {
-                    target = tempEnemy.transform;
-                    print(tempEnemy.name);
-                    ChangeDirection(target);
-                    //修改飞行时间为最大飞行时间
-                    flyTime = ConvertFlyDistanceToTime(afterbounceDistance, moveSpeed);
+                        if (tempEnemy != null)//&& tempEnemy.gameObject.activeSelf == true
+                        {
+                            target = tempEnemy.transform;
+                            print(tempEnemy.name);
+                            ChangeDirection(target);
+                            //修改飞行时间为最大飞行时间
+                            flyTime = ConvertFlyDistanceToTime(afterbounceDistance, moveSpeed);
+                        }
+                        else
+                        {
+                            print("弹射次数大于0时，未找到怪物，向随机方向发射");
+                            dir = GetRandomDirection();
+                            transform.rotation = Quaternion.LookRotation(Vector3.forward, dir);
+                            Debug.Log("随机飞行方向:" + dir);
+                            target = null;
+                            //修改飞行时间为最大飞行时间
+                            flyTime = ConvertFlyDistanceToTime(afterbounceDistance, moveSpeed);
+                        }
+
+
+                    }
+                    else
+                    {
+                        print("弹跳次数不足，销毁子弹。");
+                        gameObject.SetActive(false);
+                    }
                 }
-                else
-                {
-                    print("弹射次数大于0时，未找到怪物，向随机方向发射");
-                    dir = GetRandomDirection();
-                    transform.rotation = Quaternion.LookRotation(Vector3.forward, dir);
-                    Debug.Log("随机飞行方向:" + dir);
-                    target = null;
-                    //修改飞行时间为最大飞行时间
-                    flyTime = ConvertFlyDistanceToTime(afterbounceDistance, moveSpeed);
-                }
-            }
-            else
-            {
-                print("弹跳次数不足，销毁子弹。");
-                gameObject.SetActive(false);
             }
         }
-    }
 
+    }
 }
